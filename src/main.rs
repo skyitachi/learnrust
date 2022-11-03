20ee16a4289ad42fdf5d752ead76d7a7246b2095
Shiping Yao <shiping.yao@shopee.com>
2022-11-03 18:46:30 +0800

    add datafusion example

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,36 @@ use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-fn main() {
+use datafusion::prelude::*;
 
-    let v = vec![1, 2, 3];
-    let handle = thread::spawn(move || {
-        println!("here's is a vector: {:?}", v);
-    });
-    handle.join().unwrap();
+#[tokio::main]
+async fn main() -> datafusion::error::Result<()> {
+    // register the table
+    let ctx = SessionContext::new();
+    ctx.register_csv("example", "tests/example.csv", CsvReadOptions::new()).await?;
 
-    let listener = TcpListener::bind("localhost:10001").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream)
-    }
+    // create a plan to run a SQL query
+    let df = ctx.sql("SELECT a, MIN(b) FROM example GROUP BY a LIMIT 100").await?;
+
+    // execute and print results
+    df.show().await?;
+    Ok(())
 }
+
+// fn main() {
+//
+//     let v = vec![1, 2, 3];
+//     let handle = thread::spawn(move || {
+//         println!("here's is a vector: {:?}", v);
+//     });
+//     handle.join().unwrap();
+//
+//     let listener = TcpListener::bind("localhost:10001").unwrap();
+//     for stream in listener.incoming() {
+//         let stream = stream.unwrap();
+//         handle_connection(stream)
+//     }
+// }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
