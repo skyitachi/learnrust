41f5679e594968cfc7c041c1e9b6e84c213074a5
Shiping Yao <shiping.yao@shopee.com>
2022-12-20 10:05:09 +0800

    sync code

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,32 @@ async fn main() -> datafusion::error::Result<()> {
     Ok(())
 }
 
+#[cfg(test)]
+mod test {
+    use crate::test::List::{Cons, Nil};
+
+    enum List {
+        Cons(i32, Box<List>),
+        Nil,
+    }
+
+    #[test]
+    fn smart_pointer_demo() {
+        let b = Box::new(10);
+        println!("b = {}", b);
+        let list = Cons(1, Box::new(Nil));
+    }
+
+    #[test]
+    fn demo_15_2() {
+        let x = 5;
+        let y = Box::new(x);
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+
+}
+
 // fn main() {
 //
 //     let v = vec![1, 2, 3];
