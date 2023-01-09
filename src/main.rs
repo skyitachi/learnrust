38974517229b9d889cd93c08c9c675fe0811aa13
Shiping Yao <shiping.yao@shopee.com>
2023-01-09 18:54:50 +0800

    sync code

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ mod test {
     }
 
     fn hello(name: &str) {
-        println!("Hello {name}");
+        println!("Hello {}", name);
     }
 
     #[test]
@@ -85,8 +85,8 @@ mod test {
 }
 
 #[cfg(test)]
-
 mod test2 {
+    use std::cell::RefCell;
     use std::rc::Rc;
     use crate::test2::List::{Cons, Nil};
 
@@ -106,6 +106,164 @@ mod test2 {
             println!("count after creating c = {}", Rc::strong_count(&a));
         }
         println!("count after leave scope = {}", Rc::strong_count(&a));
+    }
+
+    // RefCell
+    pub trait Messenger {
+        fn send(&self, msg: &str);
+    }
+
+    pub struct LimitTracker<'a, T: Messenger> {
+        messenger: &'a T,
+        value: usize,
+        max: usize,
+    }
+
+    impl<'a, T> LimitTracker<'a, T>
+        where
+            T: Messenger,
+    {
+        pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+            LimitTracker {
+                messenger,
+                value: 0,
+                max,
+            }
+        }
+
+        pub fn set_value(&mut self, value: usize) {
+            self.value = value;
+
+            let percentage_of_max = self.value as f64 / self.max as f64;
+
+            if percentage_of_max >= 1.0 {
+                self.messenger.send("Error: You are over your quota!");
+            } else if percentage_of_max >= 0.9 {
+                self.messenger
+                    .send("Urgent warning: You've used up over 90% of your quota!");
+            } else if percentage_of_max >= 0.75 {
+                self.messenger
+                    .send("Warning: You've used up over 75% of your quota!");
+            }
+        }
+    }
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+
+
+}
+
+#[cfg(test)]
+mod test3 {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use crate::test3::List::{Cons, Nil};
+
+    #[derive(Debug)]
+    enum List {
+        Cons(Rc<RefCell<i32>>, Rc<List>),
+        Nil,
+    }
+
+    #[test]
+    fn test_multi_owner_list() {
+        let value = Rc::new(RefCell::new(5));
+
+        let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+        let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+        let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+        *value.borrow_mut() += 10;
+
+        println!("a after = {:?}", a);
+        println!("b after = {:?}", b);
+        println!("c after = {:?}", c);
+    }
+
+    use std::rc::Weak;
+
+    #[derive(Debug)]
+    struct Node {
+        value: i32,
+        parent: RefCell<Weak<Node>>,
+        children: RefCell<Vec<Rc<Node>>>,
+    }
+
+    #[test]
+    fn test_weak_pointer() {
+        let leaf = Rc::new(Node {
+            value: 3,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![]),
+        });
+
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+        let a = Rc::new(10);
+        let w = RefCell::new(Weak::new());
+        *w.borrow_mut() = Rc::downgrade(&a);
+    }
+}
+
+
+#[cfg(test)]
+mod test_concurrent {
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_use_spawn() {
+        let handle = thread::spawn(|| {
+           for i in 1..10 {
+               println!("run in thread {:?} with number {}", thread::current().id(), i);
+               thread::sleep(Duration::from_millis(i));
+           }
+        });
+
+        for i in 1..5 {
+            println!("run in thread {:?} with number {}", thread::current().id(), i);
+            thread::sleep(Duration::from_millis(i));
+        }
+
+        handle.join().unwrap();
     }
 }
 // fn main() {
