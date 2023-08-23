50794b364b4f35e29ec9fb3cdc9f639c079d9570
skyitachi <skbysp@gmail.com>
2023-08-23 22:30:01 +0800

    sync code

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -75,4 +75,29 @@ mod tests {
             println!("Got: {}", received);
         }
     }
+
+    #[test]
+    fn future_demo() {
+        let pool = ThreadPool::new().expect("Failed to build threadpool");
+        let (tx, rx) = mpsc::unbounded::<i32>();
+
+        let fut_values = async {
+            let fut_tx_result = async move {
+                (0..100).for_each(|v| {
+                    tx.unbound_send(v).expect("Failed to send");
+                })
+            };
+            pool.spawn_ok(fut_tx_result);
+
+            let fut_values = rx
+                .map(|v| v * 2)
+                .collect();
+
+            fut_values.await
+        };
+
+        let values: Vec<i32> = executor::block_on(fut_values);
+
+        println!("Values: {:?}", values);
+    }
 }
